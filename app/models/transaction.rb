45154edf2aba8ea1f7284229514e04ce40314554
dulguun <rockaveue@gmail.com>
2021-06-07 09:31:44 +0800

    fixed transaction_date validation

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -6,12 +6,13 @@ class Transaction < ApplicationRecord
   validates :user, presence: true
   validates :is_income, inclusion: { in: [ true, false ] }
   validates :amount, presence: true, numericality: true
+  validates :transaction_date, presence: true
   validate :transaction_date_is_valid_datetime
   validates :note, length: { maximum: 255 }
   validates :is_repeat, inclusion: { in: [true, false] }
 
   def transaction_date_is_valid_datetime
-    errors.add(:transaction_date, 'must be a valid datetime') if ((DateTime.parse(transaction_date) rescue ArgumentError) == ArgumentError)
+    errors.add(:transaction_date, 'must be a valid datetime') if ((transaction_date.is_a?(Date) rescue ArgumentError) == ArgumentError)
   end
 
   def self.getTransactionCategory(user_id)
