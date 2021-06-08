3954dd0a510cadd60b6f6ecf04956ce9672f6255
dulguun <rockaveue@gmail.com>
2021-06-08 09:14:26 +0800

    heroku email test 3 raise errors false

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,10 +37,10 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'https://your-finance-back.herokuapp.com'}
   # Don't care if the mailer can't send.
   
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
   
-  # config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
