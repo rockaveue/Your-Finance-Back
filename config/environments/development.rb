743e3be72c433cf48dd2a4822dfdd0998e7348cf
dulguun <rockaveue@gmail.com>
2021-06-08 09:24:04 +0800

    heroku email test 4

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,10 +34,10 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   
-  config.action_mailer.default_url_options = { host: 'https://your-finance-back.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: 'your-finance-back.herokuapp.com'}
   # Don't care if the mailer can't send.
   
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
   
   config.action_mailer.perform_deliveries = true
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,23 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
-
+  config.action_mailer.default_url_options = { host: 'your-finance-back.herokuapp.com'}
+  # Don't care if the mailer can't send.
+  
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_caching = false
+  
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    domain: "gmail.com",
+    port: 587,
+    user_name: ENV["EMAIL"],
+    password: ENV["EMAIL_PASSWORD"],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
