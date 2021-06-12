d5ba93739b49935fdc71311a8e0e2f49c052c4f2
dulguun <rockaveue@gmail.com>
2021-06-12 15:00:03 +0800

    heroku mailgun test 2

--- app/controllers/api/v1/users/registrations_controller.rb
+++ app/controllers/api/v1/users/registrations_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::Users::RegistrationsController < Devise::RegistrationsController
   def create
     resource = build_resource(sign_up_params)
     if resource.save
-      UserMailer.welcome_email(resource).deliver
+      UserMailer.welcome_email(resource).deliver_now
       register_success && return if resource.persisted?
     else
       render json: {message: resource.errors}, status: 422
