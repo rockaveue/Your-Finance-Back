76d5c2b085ceda67032410008dda7434d29b96ff
dulguun <rockaveue@gmail.com>
2021-06-15 10:07:36 +0800

    added env['backend_url'] for domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,6 +99,7 @@ Rails.application.configure do
     address: ENV['SMTP_SERVER_NAME'],
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
+    domain: ENV['BACKEND_URL'],
     authentication: :plain,
     enable_starttls_auto: true
   }
