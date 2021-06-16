d63c31976af6c95416d09e8469c3f043e1c95a37
dulguun <rockaveue@gmail.com>
2021-06-16 15:12:14 +0800

    mailer configuration fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,26 +86,7 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
-  config.action_mailer.default_url_options = { host: 'your-finance-back.herokuapp.com'}
-  # Don't care if the mailer can't send.
   
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.perform_caching = false
-  
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    domain: "gmail.com",
-    port: 587,
-    user_name: ENV["GMAIL_SMTP_USER"],
-    password: ENV["GMAIL_SMTP_PASSWORD"],
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
-  # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
-
   config.action_mailer.default_url_options = { host: ENV['BACKEND_URL']}
   # Don't care if the mailer can't send.
   
