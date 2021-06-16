33bb45eb7be5c29495faa59f81e50d4e9aef6881
dulguun <rockaveue@gmail.com>
2021-06-16 10:55:09 +0800

    edit and update combined

--- app/controllers/api/v1/users/passwords_controller.rb
+++ app/controllers/api/v1/users/passwords_controller.rb
@@ -8,36 +8,29 @@ class Api::V1::Users::PasswordsController < Devise::PasswordsController
     yield resource if block_given?
 
     if successfully_sent?(resource)
-      # respond_with({}, location: after_sending_reset_password_instructions_path_for(resource_name))
       render json: { message: "You will receive an email with instructions on how to reset your password in a few minutes"}
     else
       render json: { message: "Email not sent"}
     end
   end
-  # GET /api/v1/users/password/edit
-  def edit
-    begin
-      user = User.with_reset_password_token(params[:reset_password_token])
-      sign_in(user)
-      generate_new_token
-      render json: { message: "Valid token" }
-    rescue RuntimeError
-      render json: { message: "Invalid token" }
-    end
-  end
 
   # PUT /api/v1/users/password
   def update
-    @user = User.find(current_api_v1_user.id)
-    if @user.reset_password(user_params)
-      bypass_sign_in(@user)
-      render json: { message: "Password updated"}
+    self.resource = resource_class.reset_password_by_token(resource_params)
+    yield resource if block_given?
+
+    if resource.errors.empty?
+      resource.unlock_access! if unlockable?(resource)
+      if Devise.sign_in_after_reset_password
+        flash_message = resource.active_for_authentication? ? 'Password updated.' : 'Password updated. Please sign in.'
+        render json: {'message' => "#{flash_message}"}, status: 200
+        resource.after_database_authentication
+        sign_in(resource_name, resource)
+      else
+        render json: {'message' => 'Password not updated'}, status: 400
+      end
     else
-      render json: { message: "There was an error"}
+      render json: { 'message' => resource.errors }, status: 422
     end
-  end
-  private
-  def user_params
-    params.permit(:password, :password_confirmation)
   end
 end
