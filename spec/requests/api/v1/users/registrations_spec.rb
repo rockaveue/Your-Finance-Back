144ec9fefa7aee49a7b8f9b6acfd1525e4c2cd33
dulguun <rockaveue@gmail.com>
2021-06-24 11:06:28 +0800

    registration test added

--- spec/requests/api/v1/users/registrations_spec.rb
+++ spec/requests/api/v1/users/registrations_spec.rb
@@ -7,9 +7,32 @@ RSpec.describe Api::V1::Users::RegistrationsController, type: :controller do
     @user_params = attributes_for(:user)
   end
   describe "POST #create" do
-    it 'registers a user' do
+    it 'registers a user and sends email' do
       post :create, params: {api_v1_user: @user_params}
       expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body)['message']).to eq("Signed in successfully.")
+      expect(ActionMailer::Base.deliveries.size).to eq(1)
+    end
+    it 'does not registers a user without valid email' do
+      @user_params[:email] = "myemail"
+      post :create, params: {api_v1_user: @user_params}
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(JSON.parse(response.body)['message']['email']).to eq(["is invalid"])
+      expect(ActionMailer::Base.deliveries.size).to eq(0)
+    end
+    it 'does not registers a user without email' do
+      @user_params[:email] = nil
+      post :create, params: {api_v1_user: @user_params}
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(JSON.parse(response.body)['message']['email'][0]).to eq("can't be blank")
+      expect(ActionMailer::Base.deliveries.size).to eq(0)
+    end
+    it 'does not registers a user without valid first_name' do
+      @user_params[:first_name] = 'adhasdkjlfhkjasdhfkjsahdfasdfas'
+      post :create, params: {api_v1_user: @user_params}
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(JSON.parse(response.body)['message']['first_name'][0]).to eq("is too long (maximum is 30 characters)")
+      expect(ActionMailer::Base.deliveries.size).to eq(0)
     end
   end
 end
