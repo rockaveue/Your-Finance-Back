984651eb8d1739434b5bee2e87fc0a9b901a96fa
dulguun <rockaveue@gmail.com>
2021-06-25 19:26:05 +0800

    check if email exists done

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: JwtBlacklist
   
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true, presence: true, length: { maximum: 50 }
+  validates_exist_email_of :email
   validates :first_name, presence: true, length: { maximum: 30, minimum: 2 }
   validates :last_name, presence: true, length: { maximum: 30, minimum: 2 }
   validates :encrypted_password, presence: true, length: { maximum: 256 }
